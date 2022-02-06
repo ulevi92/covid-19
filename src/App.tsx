ff36c01b50a08b5ee474fd2dc0970a46b185f746
Uria <ulevi92@gmail.com>
2022-02-06 14:14:23 +0200

    created/ mui light and dark theme and applied ThemeProvider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/404";
 import About from "./pages/About";
 import Country from "./pages/Country";
 import Home from "./pages/Home";
+import { lightTheme } from "./theme/theme";
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/country' element={<Country />} />
-          <Route path='/about' element={<About />} />
+      <ThemeProvider theme={lightTheme}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/country' element={<Country />} />
+            <Route path='/about' element={<About />} />
 
-          {/*  */}
+            {/*  */}
 
-          <Route path='/404' element={<NotFound />} />
-          <Route path='/*' element={<NotFound />} />
-        </Routes>
-      </Router>
+            <Route path='/404' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,1 +1,9 @@
-export const theme = "";
+import { createTheme } from "@mui/material/styles";
+
+export let lightTheme = createTheme({});
+
+lightTheme = createTheme(lightTheme, {});
+
+export let darkTheme = createTheme({});
+
+darkTheme = createTheme(darkTheme, {});
