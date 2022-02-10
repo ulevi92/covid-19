3d32645b15bd3bcd3b055583089d16086d575fcd
Uria <ulevi92@gmail.com>
2022-02-10 15:07:38 +0200

    modified/Spinner styles with mui

--- src/components/shared/Spinner.tsx
+++ src/components/shared/Spinner.tsx
@@ -1,11 +1,24 @@
 import { Box, CircularProgress } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 const Spinner = () => {
-  return (
-    <Box sx={{ width: "100%", height: "100%", display: "flex" }}>
-      <CircularProgress />
+  const renderSpinner = (
+    <Box
+      sx={{
+        width: "99%",
+        height: "99%",
+        display: "flex",
+        justifyContent: "center",
+        position: "absolute",
+        backgroundColor: "rgba(0,0,0,0.3)",
+        alignItems: "center",
+      }}
+    >
+      <CircularProgress size={150} color='primary' />
     </Box>
   );
+
+  return <>{renderSpinner}</>;
 };
 
 export default Spinner;
